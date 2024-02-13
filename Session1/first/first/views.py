92f0d80ae880215e6248289879aa92847aa21bec
Abid Ali <49807758+Abidali08@users.noreply.github.com>
2024-02-13 12:46:26 +0000

    loop

--- Session1/first/first/views.py
+++ Session1/first/first/views.py
@@ -2,7 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def Home(request):
-    return render(request,"index.html")
+    data = {
+        'title':'Home Page',
+        'bdata':'Welcome to Edu-tech',
+        'clist':['PHP','Java','C','PYTHON'],
+        'student_details':[
+            {'name':'Ali','phone':92346797812},
+            {'name':'Hussain','phone':92312457896}
+            ]
+    }
+    return render(request,"index.html",data)
 
 def aboutus(request):
     return HttpResponse("Welcome to Eduloge")
